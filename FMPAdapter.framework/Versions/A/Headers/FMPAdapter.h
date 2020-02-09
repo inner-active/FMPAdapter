4e659c89de2915d67a889883966f854f539f486b
Nikita Fedorenko <nikita.fedorenko@fyber.com>
2020-02-09 12:26:22 +0200

    added 0.9.0

---
+++ FMPAdapter.framework/Versions/A/Headers/FMPAdapter.h
@@ -0,0 +1,40 @@
+//
+//  FMPAdapter.h
+//  FMPAdapter
+//
+//  Created by Fyber on 22/01/2020.
+//  Copyright © 2020 Inneractive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "FMPAdapterAd.h"
+#import "FMPAdapterAuctionParameters.h"
+#import "FMPAdapterAdData.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSUInteger) {
+    FMPLoggingLevelLevelDisabled,
+    FMPLoggingLevelLevelError,
+    FMPLoggingLevelLevelDebug,
+} FMPLoggingLevel;
+
+@interface FMPAdapter : NSObject
+
+/**
+ *  @brief Singleton method, use for any instance call.
+ */
++ (instancetype _Null_unspecified)sharedInstance;
+
+- (void)initWithAppId:(nonnull NSString *)appId FairBidSDKVersion:(nonnull NSString *)version logLevel:(FMPLoggingLevel)logLevel;
+
+- (nullable FMPAdapterAuctionParameters *)auctionParameters;
+- (nonnull NSString *)userAgent;
+
+- (id<FMPAdapterBannerAd>)bannerAd;
+- (id<FMPAdapterFullscreenAd>)fullscreenAd;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FMPAdapter.framework/Versions/A/Headers/FMPAdapterAd.h
@@ -0,0 +1,86 @@
+//
+//  FMPAdapterAd.h
+//  FMPAdapter
+//
+//  Created by Fyber on 28/01/2020.
+//  Copyright © 2020 Inneractive. All rights reserved.
+//
+
+#ifndef FMPAdapterAd_h
+#define FMPAdapterAd_h
+
+#import <UIKit/UIKit.h>
+
+@class FMPAdapterAdData;
+
+#pragma mark - Adapter Side
+
+@protocol FMPAdapterAd <NSObject>
+
+@property (nonatomic, weak, nullable) id delegate;
+
+@required
+- (void)load:(nonnull FMPAdapterAdData *)adData;
+
+@end
+
+@protocol FMPAdapterBannerAdDelegate;
+@protocol FMPAdapterBannerAd <FMPAdapterAd>
+
+@required
+@property (nonatomic, weak, nullable) id<FMPAdapterBannerAdDelegate> delegate; // override
+@property (nonatomic, readonly) BOOL canRefresh;
+
+@end
+
+@protocol FMPAdapterFullscreenAdDelegate;
+@protocol FMPAdapterFullscreenAd <FMPAdapterAd>
+
+@required
+@property (nonatomic, weak, nullable) id<FMPAdapterFullscreenAdDelegate> delegate; // override
+ 
+- (void)showInViewController:(nullable UIViewController *)viewController;
+ 
+@end
+
+#pragma mark - Client Side
+
+@protocol FMPAdapterAdDelegate <NSObject>
+
+@required
+- (void)adDidFailToLoad:(nullable NSError *)error;
+
+- (void)adDidShow;
+- (void)adDidFailToShow;
+- (void)adDidClick;
+
+- (void)adWillPresentModalView;
+- (void)adDidDismissModalView;
+
+- (void)adWillLeaveApplication;
+
+@end
+
+@protocol FMPAdapterBannerAdDelegate <FMPAdapterAdDelegate>
+
+@required
+- (UIViewController * _Nonnull)parentViewControllerForModalView;
+
+- (void)adDidLoadView:(nonnull UIView *)view;
+ 
+- (void)adDidResizeToFrame:(CGRect)frame;
+ 
+@end
+
+@protocol FMPAdapterFullscreenAdDelegate <FMPAdapterAdDelegate>
+ 
+@required
+- (void)adDidLoad;
+
+- (void)adDidDismiss;
+- (void)adDidComplete;
+- (void)adDidFailToCompleteWithError:(nullable NSError *)error;
+ 
+@end
+
+#endif /* FMPAdapterAd_h */
